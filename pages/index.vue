909ff6157eb42bcd535a15893ae82f1515b4e1b0
Azariah <azariah.pundari@my.jcu.edu.au>
2024-03-12 20:01:42 +1000

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script>
+export default {}
+</script>
+
+<template>
+  <section>
+    <h1 class="text-5xl font-bold mt-20">Hello, I'm Azariah</h1>
+    <h2 class="text-base text-gray-900 p-2 italic">Local Tech Wizard</h2>
+  </section>
+</template>
+
+<style scoped></style>
