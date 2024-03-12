4d67a1efccb03f8d82aab0c212b1e09dfa22a821
Azariah <azariah.pundari@my.jcu.edu.au>
2024-03-12 20:01:15 +1000

    reformat modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
-  modules: [ 
-    '@nuxtjs/tailwindcss'
-  ],
+  modules: ['@nuxtjs/tailwindcss'],
   devtools: { enabled: true }
 })
