cf5d6c04b72c9cf1372ae70e9962c3e64b78e20d
Azariah <azariah.pundari@my.jcu.edu.au>
2024-03-14 02:42:31 +1000

    refactor text colour

--- pages/index.vue
+++ pages/index.vue
@@ -4,16 +4,14 @@ export default {}
 
 <template>
   <section>
-    <h1 class="text-5xl text-green-600 font-bold mt-20">Hello, I'm Azariah</h1>
-    <h2 class="text-3xl text-green-600 italic font-medium">
-      Local Tech Wizard
-    </h2>
+    <h1 class="text-5xl text-stone-100 font-bold mt-20">Hello, I'm Azariah</h1>
+    <h2 class="text-3xl text-stone-100 font-medium">Local Tech Wizard 🧙‍♂️</h2>
   </section>
 
   <section class="flex flex-col md:flex-row">
     <div class="md:w-3/4">
-      <h2 class="text-3xl font-bold mt-8 text-green-500">My Bio</h2>
-      <p class="text-green-500 text-lg py-5 mr-11 text-justify">
+      <h2 class="text-3xl font-bold mt-8 text-stone-100">My Bio</h2>
+      <p class="text-stone-100 text-lg py-5 mr-11 text-justify">
         I am in my final semester of university, studying a Bachelor of
         Information Technology. I strive to become a full-stack developer and an
         ethical hacker as I found them to be most interesting during my studies.
@@ -22,20 +20,20 @@ export default {}
         art or anything that I can get my hands on.
       </p>
 
-      <p class="text-green-500 text-lg py-5 mr-11 text-justify">
+      <p class="text-stone-100 text-lg py-5 mr-11 text-justify">
         Check out what I've been working on 😎
       </p>
     </div>
 
     <img
-      class="rounded-full w-80 h-80 sm:max-h-96 sm:rounded-full p-5 mx-auto"
+      class="rounded-full w-80 h-80 p-5 mx-auto"
       src="../assets/images/proflile.jpg"
       alt="My profile picture"
     />
   </section>
 
   <section>
-    <h2 class="text-3xl font-bold mt-8 text-green-600">
+    <h2 class="text-3xl font-bold mt-8 text-stone-100">
       Latest Learning Activities
     </h2>
   </section>
