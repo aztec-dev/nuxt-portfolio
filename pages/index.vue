464128e792e32f281380ee762fb91846f6fd41d6
Azariah <azariah.pundari@my.jcu.edu.au>
2024-03-14 02:00:38 +1000

    add headshot, content

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,33 @@ export default {}
 <template>
   <section>
     <h1 class="text-5xl text-green-600 font-bold mt-20">Hello, I'm Azariah</h1>
-    <h2 class="text-3xl text-green-600 p-2 italic font-semibold">Local Tech Wizard</h2>
+    <h2 class="text-3xl text-green-600 italic font-medium">
+      Local Tech Wizard
+    </h2>
+  </section>
+
+  <section class="flex flex-col md:flex-row">
+    <div class="md:w-3/4">
+      <h2 class="text-3xl font-bold mt-8 text-green-500">My Bio</h2>
+      <p class="text-green-500 text-lg py-5 mr-11 text-justify">
+        I am in my final semester of university, studying a Bachelor of
+        Information Technology. I strive to become a full-stack developer and an
+        ethical hacker as I found them to be most interesting during my studies.
+        I am most passionate about learning how things work, especially with
+        technology and software, and building things - whether it be programs,
+        art or anything that I can get my hands on.
+      </p>
+    </div>
+
+    <img
+      class="rounded-full w-80 h-80 sm:max-h-96 sm:rounded-full p-5 mx-auto"
+      src="../assets/images/proflile.jpg"
+      alt="My profile picture"
+    />
+  </section>
+
+  <section>
+    <h2 class="text-3xl font-bold mt-8">Latest Learning Activities</h2>
   </section>
 </template>
 
