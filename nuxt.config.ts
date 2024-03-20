c079d651695dc715f8d8d3fef0dcd5b7678448c4
Azariah <azariah.pundari@my.jcu.edu.au>
2024-03-20 23:54:55 +1000

    refactor code layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon'],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxt/content'],
+  content: {
+    highlight: {
+      theme: 'nord',
+      preload: ['ts', 'js', 'html', 'css', 'json', 'yaml', 'md', 'markdown']
+    }
+  },
   devtools: { enabled: true }
 })
