2d20aa5aef17f1b146fe1eba25bf848322b539ce
Azariah <azariah.pundari@my.jcu.edu.au>
2024-03-20 23:56:46 +1000

    initial commit

---
+++ pages/learning_journal/[...slug].vue
@@ -0,0 +1,14 @@
+<script setup>
+const { path } = useRoute()
+
+const { data } = await useAsyncData(`content-${path}`, () => {
+  return queryContent().where({ _path: path }).findOne()
+})
+console.log(path)
+</script>
+
+<template>
+  <main>
+    <ContentDoc class="prose"></ContentDoc>
+  </main>
+</template>
